37b16a42eb4e532991ea7d3ccdb423388585ee07
Chen <75549707+chengege666@users.noreply.github.com>
2025-10-05 19:28:06 +0800

    Update vpsgj.sh

--- vpsgj.sh
+++ vpsgj.sh
@@ -12,8 +12,10 @@ UNINSTALL_NOTE="vps_toolbox_uninstall_done.txt"
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
 MAGENTA="\033[1;35m"
 CYAN="\033[1;36m"
+WHITE="\033[1;37m"
 RESET="\033[0m"
 
 print_welcome() {
